0b37265a1a7c0bf06e57ccff3b624053358a3f9c
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-05-30 22:04:17 -0500

    Final toggle dark mode

---
+++ leonidas/social-media-dashboard/js/app.js
@@ -0,0 +1,10 @@
+const checkbox = document.querySelector('#checkbox');
+
+if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+    checkbox.setAttribute('checked', true);
+}
+
+checkbox.addEventListener('change', function(event){
+    document.body.classList.toggle('is-dark-mode');
+   
+});
